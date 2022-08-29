d8d2761a737deec45c98c7c1467f4127a47f0833
Rucach <98107806+rucach@users.noreply.github.com>
2022-08-29 19:34:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function preload(){
+}
+
+function setup(){
+    canvas = createCanvas(500, 500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(500, 500);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+function modelLoaded(){
+    console.log("poseNet Initilized");
+}
+
+function draw(){
+    image(video, 0, 0, 500, 500);
+}
+
+function take_snapshot(){
+}
+
+function draw(){
+    image(video, 0, 0, 500, 500);
+}
+
+function take_snapshot(){
+    save("image.png");
+}
+
+function gotPoses(results){
+    if(results.lenght > 0){
+        console.log(results);
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+    }
+}
